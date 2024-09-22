3c3f6a9a381097c50e3590580901cb08873156bb
MRSgkce <mursidegokce985@gmail.com>
2024-09-22 17:57:54 +0300

    3 adet buton stack içine eklenrek düzgün görünüm sağlandı ve butonlar kodlara bağlandı.

--- bootcamp3/ViewController.swift
+++ bootcamp3/ViewController.swift
@@ -14,6 +14,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func butonAlert(_ sender: Any) {
+        
+    }
+    
+    @IBAction func butonAction(_ sender: Any) {
+    }
+    @IBAction func butonOzel(_ sender: Any) {
+    }
 }
 
